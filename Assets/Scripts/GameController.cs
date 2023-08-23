cc8a74ee7c3aa8bed9dc841dc275f204c6cf2fca
Guilherme de Deus <kingbowserii@gmail.com>
2023-08-23 11:12:00 -0300

    Adding scripts to support game controller throughout the game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class responsible for keeping all game info
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    List<MemoryEnum> memoriesSeen = new List<MemoryEnum>();
+
+    [SerializeField]
+    CharacterEnum currentCharacterDive;
+
+    public static GameController controller;
+
+    public CharacterEnum CurrentCharacterDive { get => currentCharacterDive; }
+
+    void Awake() {
+        if (controller == null) {
+            controller = this;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void NewMemorySeen(MemoryEnum memory) {
+        memoriesSeen.Add(memory);
+    }
+
+    public void DiveIntoCharacterMemory(CharacterEnum character) {
+        currentCharacterDive = character;
+
+        // Call minigame scene
+    }
+
+    public bool IsMemorySeen(MemoryEnum memory) {
+        return memoriesSeen.Contains(memory);
+    }
+
+    public void SubmitCulprit(CharacterEnum character) {
+        // TODO Prepare code to send to ending
+    }
+}
