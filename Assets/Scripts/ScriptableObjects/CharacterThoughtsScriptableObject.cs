3054629c6724065387ad9801563edf385b5be67d
Guilherme de Deus <kingbowserii@gmail.com>
2023-08-24 16:56:25 -0300

    Creating the scriptable object for all characters

--- Assets/Scripts/ScriptableObjects/CharacterThoughtsScriptableObject.cs
+++ Assets/Scripts/ScriptableObjects/CharacterThoughtsScriptableObject.cs
@@ -6,13 +6,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CharacterThoughts", menuName = "ScriptableObjects/CharacterThoughts")]
 public class CharacterThoughtsScriptableObject : ScriptableObject
 {
-    [Serializable]
-    public class IrrelevantThought {
-        // Maps a thought to a feeling, this way the feeling is what is in the thought balloon
-        public string feeling;
-        public string thought;
-    }
-
     public CharacterEnum character;
 
     public List<IrrelevantThought> thoughtsList;
---
+++ Assets/Scripts/ScriptableObjects/IrrelevantThought.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class IrrelevantThought {
+    // Maps a thought to a feeling, this way the feeling is what is in the thought balloon
+    public string feeling;
+    public string thought;
+}
