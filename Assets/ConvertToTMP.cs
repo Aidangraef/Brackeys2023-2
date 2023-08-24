59eaa49b0df4d508e2880432aef9162205e0de7d
AidanGraef31 <aidantgraef@gmail.com>
2023-08-23 21:14:23 -0400

    made sound dialogue thingy

--- Assets/ConvertToTMP.cs
+++ Assets/ConvertToTMP.cs
@@ -7,16 +7,39 @@ public class ConvertToTMP : MonoBehaviour
 {
     public Text text;
 
+    int i;
+    bool talking;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        i = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.GetComponent<TMPro.TextMeshProUGUI>().text = text.text;
-        //AkSoundEngine.PostEvent("npcTalk", this.gameObject);
+        if(text.text != this.GetComponent<TMPro.TextMeshProUGUI>().text)
+        {
+            this.GetComponent<TMPro.TextMeshProUGUI>().text = text.text;
+            talking = true;
+        }
+        else
+        {
+            talking = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(i >= 3 && talking)
+        {
+            AkSoundEngine.PostEvent("npcTalk", this.gameObject);
+            i = 0;
+        }
+        else
+        {
+            i += 1;
+        }
     }
 }
