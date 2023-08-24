96a8a49d0ead084ac353bd8785d55f76d7331fbd
KhaoKG <Evanseamans@gmail.com>
2023-08-24 03:15:28 -0700

    Full Audio System Minus Other Scenes
    Modify other Dialogue Managers or Maintain Current, either should work.

--- Assets/ConvertToTMP.cs
+++ Assets/ConvertToTMP.cs
@@ -7,10 +7,6 @@ using UnityEngine.UI;
 public class ConvertToTMP : MonoBehaviour
 {
     public Text text;
-    public string currentSpeaker;
-    public int speechFreq = 0;
-    public int i = 0;
-    public bool talking;
 
     // Start is called before the first frame update
     void Awake()
@@ -29,62 +25,6 @@ public class ConvertToTMP : MonoBehaviour
         if (text.text != this.GetComponent<TMPro.TextMeshProUGUI>().text)
         {
             this.GetComponent<TMPro.TextMeshProUGUI>().text = text.text;
-            talking = true;
-        }
-        else
-        {
-            talking = false;
-        }
-        int actorID = DialogueManager.currentConversationState.subtitle.speakerInfo.id;
-        if (actorID == 1)
-        {
-            currentSpeaker = "Player";
-            AkSoundEngine.PostEvent("npcPlayer", this.gameObject);
-        }
-        else if (actorID == 2)
-        {
-            currentSpeaker = "Barkeep";
-            AkSoundEngine.PostEvent("npcBarkeep", this.gameObject);
-        }
-        else if (actorID == 3)
-        {
-            currentSpeaker = "Ben";
-            AkSoundEngine.PostEvent("npcBen", this.gameObject);
-            speechFreq = 6;
-        }
-        else if (actorID == 4)
-        {
-            currentSpeaker = "Tina";
-            AkSoundEngine.PostEvent("npcTina", this.gameObject);
-            speechFreq = 6;
-        }
-        else if (actorID == 5)
-        {
-            currentSpeaker = "Kevin";
-            AkSoundEngine.PostEvent("npcKevin", this.gameObject);
-            speechFreq = 4;
-        }
-        else if (actorID == 6)
-        {
-            currentSpeaker = "Wally";
-            AkSoundEngine.PostEvent("npcWally", this.gameObject);
-        }
-        else
-        {
-            Debug.LogWarning($"Dialogue System: Actor is Blank");
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if(i >= speechFreq && talking)
-        {
-            AkSoundEngine.PostEvent("npcTalk", this.gameObject);
-            i = 0;
-        }
-        else
-        {
-            i += 1;
         }
     }
 }
